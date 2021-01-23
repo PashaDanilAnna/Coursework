b8fcf35ea309423a7cf42223592346bbbfd634e3
AnnaVorobyeva531 <anya.vorobyeva.2015@gmail.com>
2021-01-23 16:45:41 +0500

    Вызов функции в файле задания внутри блока if __name__ == '__main__'

--- main.py
+++ main.py
@@ -32,3 +32,59 @@ def load_weights():
         W[i] = np.array(W1)
         outs.append([])
     my_file.close()
+
+
+if __name__ == '__main__':
+    l1 = 0
+    while l1 != 1 and l1 != 2:
+        print("От куда загрузить веса для нейросети")
+        print("1) Файл")
+        print("2) Сгенерировать")
+        l1 = int(input("Выберите действие"))
+    if l1 == 1:
+        print("Загрузка нейросети из файла")
+        load_weights()
+    if l1 == 2:
+        random_weighs()
+        print("Веса сгенерированы")
+
+    l2 = 0
+    while l2 != 3:
+        print("1) Обучить нейросеть")
+        print("2) Найти по ключевым словам УДК")
+        print("3) Выйти")
+        l2 = int(input("Выберите действие"))
+        if l2 == 1:
+            print("Обучение")
+            load_data()  # загрузка данных для обучения из файла
+            train(epoch)
+            l3 = 0
+            while l3 != 4:
+                print("Выберите дейчтвие")
+                print("1) Сохранить веса")
+                print("2) Сбросить веса")
+                print("3) Повторить обучение")
+                print("4) Выйти в главное меню")
+                l3 = int(input("Выберите действие"))
+                if l3 == 1:
+                    print("Сохранение весов")
+                    save_weighs()
+                if l3 == 2:
+                    random_weighs()
+                    print("Веса сброшены")
+                if l3 == 3:
+                    print("Повторение обучения")
+                    train(epoch)
+
+        if l2 == 2:
+            print(
+                "Введите ключевые слова через запятую с пробелами в одну строку. Например: нейронная сеть, информатика, ...")
+            list_str = str(input()).split(",")
+            str = []
+            for i in range(count_inputs):
+                if (i < len(list_str)):
+                    str.append(get_ASCII_fron_str(list_str[i]))
+                else:
+                    str.append('0')
+            y, out = go_forward(str)
+            print(y)
