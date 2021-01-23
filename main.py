85ade268e42a422c5342fac48b926ca13467e31c
DanilKuz73 <77884613+DanilKuz73@users.noreply.github.com>
2021-01-23 18:43:19 +0500

    Функция создания весов

--- main.py
+++ main.py
@@ -11,7 +11,21 @@ def save_weighs():
     my_file.writelines(temp_W)
     my_file.close()
 
-
+def random_weighs():
+    global W
+    W = [] 
+    prew_count_neuron_layer = count_inputs
+    for i in range(len(count_neuron_in_layers)):
+        W1 = []
+        for j in range(count_neuron_in_layers[i]):
+            W2 = []
+            for k in range(prew_count_neuron_layer):
+                W2.append(float(rand.uniform(-0.5,0.5)))   
+            W1.append(W2) 
+        W.append(W1) 
+        outs.append([])  
+        prew_count_neuron_layer = count_neuron_in_layers[i] 
+        
 def load_weights():
     global W
     W = []
